26095373e6b146941af139f739a7ef4b1e8e9849
Pedro Lourenço <murilomatias410@gmail.com>
2026-05-05 22:02:34 -0300

    Add Sculptor class with basic methods
    Implement Sculptor class with constructor, destructor, and setColor method.

---
+++ sculptor.cpp
@@ -0,0 +1,15 @@
+#include "sculptor.h"
+#include <iostream>
+
+Sculptor::Sculptor(int _nx, int _ny, int _nz){//construtor
+  
+}
+Sculptor::~Sculptor(){//destrutor
+  
+}
+void Sculptor::setColor(float r, float g, float b, float a){
+  
+}
+
+
+
